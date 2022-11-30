a9abc3cf4c511ee69ddb542c78567051535c782c
Lyndon Hill <lh@lyndonhill.com>
2022-11-30 21:52:28 +0000

    Added an optional TOML header

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ reading. It's easy to grep your notes.
   - No, using YYYYmmdd is the only way to make sure that files are automatically
     listed in chronological order
 - If you run `note.sh` without a title, it will use `note` for the title
+- `note.sh` adds a TOML header that can be disabled
 - If you use multiple keywords with `listnotes.sh` they will be logical OR, i.e. any file
   (name or contents) that contains any of the keywords will be output
 - `listnotes.sh` lists notes in order recent to oldest but if more than one keyword is specified then the ordering may not be correct
--- note.sh
+++ note.sh
@@ -4,18 +4,29 @@
 # Author : Lyndon Hill
 # Brief  : Make a note
 
+# Set this to 1 to add an optional header, 0 to disable
+ADDHEADER=1
+
 # Get note name or default to "note"
 TITLE="note"
 if [ $# -gt 0 ]; then
   TITLE="$1"
 fi
 
-NOTENAME="${HOME}/Documents/Notes/${TITLE}$(date +"%Y%m%d")"
+DATE=$(date +"%Y%m%d")
+NOTENAME="${HOME}/Documents/Notes/${TITLE}${DATE}"
 
 if test -f "$NOTENAME"; then
   echo "File $NOTENAME already exists"
   exit 1
 fi
 
+if [ $ADDHEADER -gt 0 ]; then
+  # Add header
+  HEADERDATE=$(date +"%Y-%m-%dT%T")
+  printf "title = ${TITLE}\ndate = ${HEADERDATE}\n" >> ${NOTENAME}
+  printf "tags =\npath = ${NOTENAME}\n\n\n" >> ${NOTENAME}
+fi
+
 # Set your favourite text editor here
-gvim ${NOTENAME}
+gvim + ${NOTENAME}
